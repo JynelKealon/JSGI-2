880e30ffad599e6360b2cf944305cc36bed95552
Jynel Kealon <jkealon13@r2happrentice.org>
2023-04-25 14:18:44 -0400

    new changes

---
+++ VeryEasy/veryeasy.js
@@ -0,0 +1,10 @@
+
+function minNum (firstNum,secondNum){
+    if (firstNum< secondNum)
+    return firstNum;
+    else
+    return secondNum;
+    
+}
+console.log("Minimum number is");
+console.log(minNum(5, 2));
