b2e9fe26b08a85acc58f28eaf25ad94640785c21
Jynel Kealon <jkealon13@r2happrentice.org>
2023-04-25 14:15:09 -0400

    new changes

---
+++ Hard/hard.js
@@ -0,0 +1,22 @@
+// Tom (Data 1)
+let tomMass = 0.008
+let tomHeight = 0.2286
+
+let c = tomMass / tomHeight ** 2;
+
+
+// Jerry (Data 1)
+
+let jerryMass = 0.045
+let jerryHeight = 0.254
+
+let d = jerryMass / jerryHeight ** 2;
+
+//Boolean 
+
+jerryHigherBMI = c < d
+
+console.log("Is Tom's BMI higher than Jerry's?" + " " + jerryHigherBMI)
+
+
+
