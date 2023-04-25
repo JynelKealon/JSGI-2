680ff21f23b5080c6fd3847a5c8137f25b1a3e1e
Jynel Kealon <jkealon13@r2happrentice.org>
2023-04-25 14:17:59 -0400

    New changes

---
+++ Easy/easy.js
@@ -0,0 +1,16 @@
+const firstName = [];
+firstName[0] = "Mark";
+firstName[1] = "John";
+firstName[2] = "Jane";
+
+const lastName = []
+lastName[0] = "Johnson";
+lastName[1] = "Kealon";
+lastName[2] = "Davis";
+
+const studentAge = []
+studentAge[0] = 21;
+studentAge[1] = 22;
+studentAge[2] = 23;
+
+console.log("Hello, my name is" + " " + firstName[1] + " " + lastName[1] + " "  + "and I'm" + " " + studentAge[1] +  " " + "years old");
